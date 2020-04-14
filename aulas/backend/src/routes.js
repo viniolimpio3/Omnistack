8f7e28881d5f1783c82e10d76febc10c0fba590f
viniOlimpio <viniolimpio3@gmail.com>
2020-04-14 17:41:33 -0300

    database migrations created

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -3,33 +3,35 @@ const express = require('express');
 const routes = express.Router();
 
 //resumo de metodos http e requisições(request) e respostas(response)
-    /*
-        https://ROTA/RECURSO("PAGINAS")
+/*
+    https://ROTA/RECURSO("PAGINAS")
 
-    */
+*/
 
-    /* MÉTODOS HTTP - (REQUISIÇÕES) Quando for criar uma rota ( se eu usar somente o get, funciona, mas por uma questão de semântica existem essas diferenças )
-        (Navegador usa apenas o método get - é o 'que dá pra ver')
+/* MÉTODOS HTTP - (REQUISIÇÕES) Quando for criar uma rota ( se eu usar somente o get, funciona, mas por uma questão de semântica existem essas diferenças )
+    (Navegador usa apenas o método get - é o 'que dá pra ver')
 
-        - GET - Buscar/Listar informações do backend .get('/recurso', function de retorno) - mostra na url
-        - POST - Criar informações no backend 
-        - PUT - Alterar informações no backend
-        - DELETE - Deletar informações no backend
-    */
+    - GET - Buscar/Listar informações do backend .get('/recurso', function de retorno) - mostra na url
+    - POST - Criar informações no backend 
+    - PUT - Alterar informações no backend
+    - DELETE - Deletar informações no backend
+*/
 
-    /** PARÂMETROS PASSADOS PELAS ROTAS
-     * 
-     *  - Query Params: Parâmetros NOMEADOS enviados na rota seguido do sinal "?" (Serve p Filtros e Listas p/ exemplo)
-     *      exemplo rota.com\recurso?aluno=Vinicius&evento=Omnistack -> traz todos os alunos com nome Vinicius....
-     * 
-     *  - Route Params: Parâmetros utilizados para identificar RECURSOS -> traz um perfil com um ÚNICO id p exemplo
-     *      exemplo rota.com\recurso\{id} -> não preciso nomear qual entidade estou buscando, como nos query params
-     * 
-     *  - Request Body: Corpo da requisição, utilizado para criar ou alterar recursos (busca todo o corpo da requisição)
-     */
- 
-    //request -> guarda todos os dados que vem da requisição feita na view!
-    //response -> resposta ao usuário
+/** PARÂMETROS PASSADOS PELAS ROTAS
+ * 
+ *  - Query Params: Parâmetros NOMEADOS enviados na rota seguido do sinal "?" (Serve p Filtros e Listas p/ exemplo)
+ *      exemplo rota.com\recurso?aluno=Vinicius&evento=Omnistack -> traz todos os alunos com nome Vinicius....
+ * 
+ *  - Route Params: Parâmetros utilizados para identificar RECURSOS -> traz um perfil com um ÚNICO id p exemplo
+ *      exemplo rota.com\recurso\{id} -> não preciso nomear qual entidade estou buscando, como nos query params
+ * 
+ *  - Request Body: Corpo da requisição, utilizado para criar ou alterar recursos (busca todo o corpo da requisição)
+ */
+
+//request -> guarda todos os dados que vem da requisição feita na view!
+//response -> resposta ao usuário
+
+
 
 
 routes.post('/users', (request, response)=> {//arow function -> ("requisição", "resposta") => {'função'}
